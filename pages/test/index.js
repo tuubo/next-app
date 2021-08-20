b9b17b343c002248da422286c1727c6a2affaca7
unknown <tubo260.qd@gmail.com>
2021-08-20 23:56:16 +0900

    [feat] Add function for SSR

--- pages/test/index.js
+++ pages/test/index.js
@@ -1,5 +1,22 @@
-function Test() {
-  return <div>This is test page!!</div>
+// すべてのリクエストの度に実行される
+export async function getServerSideProps() {
+  // 外部APIからデータフェッチ
+  const res = await fetch(`https://zipcloud.ibsnet.co.jp/api/search?zipcode=7830060`)
+  const data = await res.json()
+
+  // props を通じて Page に data を渡す
+  return { props: { data } }
+}
+
+function Test({ data }) {
+  const results = data.results;
+
+  return (
+    <main>
+      <div>This is test page!!</div>
+      <div>{results[0].address1}</div>
+    </main>
+  );
 }
 
 export default Test;
