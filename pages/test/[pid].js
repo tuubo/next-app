92a561cd7a956fbb0d1e3ce66b906e051546b675
unknown <tubo260.qd@gmail.com>
2021-08-25 23:10:26 +0900

    [feat] Change test page url to dynamic route

--- pages/test/[pid].js
+++ pages/test/[pid].js
@@ -1,20 +1,27 @@
+import { useRouter } from 'next/router';
+
 // すべてのリクエストの度に実行される
 export async function getServerSideProps() {
   // 外部APIからデータフェッチ
-  const res = await fetch(`https://zipcloud.ibsnet.co.jp/api/search?zipcode=7830060`)
-  const data = await res.json()
+  const res = await fetch(`https://zipcloud.ibsnet.co.jp/api/search?zipcode=7830060`);
+  const data = await res.json();
 
   // props を通じて Page に data を渡す
-  return { props: { data } }
+  return { props: { data } };
 }
 
 function Test({ data }) {
+  const router = useRouter();
+  const { pid } = router.query;
   const results = data.results;
 
   return (
     <main>
       <div>This is test page!!</div>
       <div>{results[0].address1}</div>
+      <div>
+        <p>{pid}</p>
+      </div>
     </main>
   );
 }
